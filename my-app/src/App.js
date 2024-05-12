dea14e996ad178a29788308f800875b4131fa504
Shubham Singh <shubhammahar1306@gmail.com>
2024-05-12 11:58:49 +0530

    Blog Hub

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -104,7 +104,7 @@ const App = () => {
 
   return (
     <div>
-      <h1 className="title">Welcome to Tech Blogs!</h1>
+      <h1 className="title">Blog Hub</h1>
 
       <SearchForm
         searchTerm={searchTerm}
